597a4d2cee0309412b66ee33717c5c6c16f91c70
zqf <3124475397@qq.com>
2025-06-04 13:05:53 +0800

    14

--- code/src/Motor.c
+++ code/src/Motor.c
@@ -21,6 +21,8 @@ void Motor_Control(unsigned char cmd) {
         M4A = 1; M4B = 0; // 右后正转
         LED_2(LED_ON);
         DelayMs(1);
+        Motor_Init();
+        DelayMs(9);
     }
     else if(cmd == RIGHT){
         M1A = 1; M1B = 0; // 左前正转
@@ -29,6 +31,8 @@ void Motor_Control(unsigned char cmd) {
         M4A = 0; M4B = 1; // 右后反转
         LED_4(LED_ON);
         DelayMs(1);
+        Motor_Init();
+        DelayMs(6);
     }
     else if(cmd == LEFT){
         M1A = 0; M1B = 1; // 左前反转
@@ -37,6 +41,8 @@ void Motor_Control(unsigned char cmd) {
         M4A = 1; M4B = 0; // 右后正转
         LED_3(LED_ON);
         DelayMs(1);
+        Motor_Init();
+        DelayMs(6);
     }
     else if(cmd == STOP){
         M1A = 0; M1B = 0;
@@ -45,13 +51,7 @@ void Motor_Control(unsigned char cmd) {
         M4A = 0; M4B = 0;
         LED_5(LED_ON);
         LED_6(LED_ON);
-        DelayMs(1);
-
+        DelayMs(10);
     }          
-    M1A = 0; M1B = 0;
-    M2A = 0; M2B = 0;
-    M3A = 0; M3B = 0;
-    M4A = 0; M4B = 0;
-    DelayMs(9);
     LED_Init();
 }
